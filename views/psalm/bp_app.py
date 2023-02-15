489abc8d5be2cc44cf92c18908902e8d5513f8dd
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-02-15 11:58:05 +0800

    asd

--- views/psalm/bp_app.py
+++ views/psalm/bp_app.py
@@ -980,44 +980,6 @@ def spreadsheet():
 
 
 
-@app.route("/resp_prof")
-def resp_prof():
-    return render_template("resp_prof.html")
-
-
-@app.route("/bsns_info")
-def bsns_info():
-    return render_template("bsns_info.html")
-
-@app.route("/ciboasn")
-def ciboasn():
-    return render_template("ciboasn.html")
-
-@app.route("/dcf/dcf_trade_promotion")
-def dcf_trade_promotion():
-    return render_template("dcf_trade_promotion.html")
-
-
-@app.route("/dcf/dcf_product_development")
-def dcf_product_development():
-    return render_template("dcf_product_development.html")
-
-@app.route("/dcf/dcf_matching_grant")
-def dcf_matching_grant():
-    return render_template("dcf_matching_grant.html")
-
-@app.route("/dcf/dcf_enablers_tracker")
-def dcf_enablers_tracker():
-    return render_template("dcf_enablers_tracker.html")
-
-@app.route("/dcf/dcf_equity_tracker")
-def dcf_equity_tracker():
-    return render_template("dcf_equity_tracker.html")
-
-@app.route("/dcf/dcf_dip_preparation_tracker")
-def dcf_dip_preparation_tracker():
-    return render_template("dcf_dip_preparation_tracker.html")
-
 if __name__ == "__main__":
     app.run(debug=True)
 
