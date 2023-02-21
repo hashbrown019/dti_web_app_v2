2844a57b20096bdbc07f135119a682120473ffc0
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2023-02-21 17:04:22 +0800

    Update inbound.py

--- controllers/inbound.py
+++ controllers/inbound.py
@@ -65,7 +65,8 @@ class data_cleaning:
 
 		for count in range(len(ress)-1):
 			for ky in ress[count]:
-				if(type(ress[count][ky])=="bytearray" or type(ress[count][ky])=="bytes"):
+				print("{} = {}".format(ky,type(ress[count][ky])))
+				if(type(ress[count][ky]) in ["bytes","bytearray"]):
 					print("true = "+ress[count][ky] )
 					ress[count][ky] = ress[count][ky].decode("utf-8")
 		print(ress)
