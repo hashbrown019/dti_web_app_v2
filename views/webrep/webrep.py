28cba3d35a341e3848e0ea8f2e3b6223b7406708
cjmontanez1997 <117901200+cjmontanez1997@users.noreply.github.com>
2023-03-06 13:44:30 +0800

    Updating Multimedia & Publication pages

--- views/webrep/webrep.py
+++ views/webrep/webrep.py
@@ -45,7 +45,16 @@ class _main:
 				return render_template("{}/{}".format(segment,page),users=_main.get_all_user(),is_session =_main.is_on_session(),user_data=session["USER_DATA"][0],upload_file_webrep=_main.get_uploads_docs())
 			else:
 				return redirect("/login?force_url=1")
-		elif(page.lower()=="document.html".lower() or page.lower()=="multimedia.html".lower() or page.lower()=="publication.html".lower()):
+		elif(
+			page.lower()=="document.html".lower() or 
+			page.lower()=="multimedia.html".lower() or 
+			page.lower()=="publication.html".lower() or 
+			page.lower()=="about.html".lower() or 
+			page.lower()=="NewsAndStories.html".lower()or 
+			page.lower()=="adminKnowledgeCenter.html".lower()or 
+			page.lower()=="knowledgecenter.html".lower()or 
+			page.lower()=="scope.html".lower()
+			):
 			if(_main.is_on_session()):
 				return render_template("{}/{}".format(segment,page),users=_main.get_all_user(),is_session =_main.is_on_session(),user_data=session["USER_DATA"][0])
 			else:
