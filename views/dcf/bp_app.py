313ba13bcf5636396e4345e265f2c5a9dbadfea6
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-03-13 17:19:15 +0800

    asd

--- views/dcf/bp_app.py
+++ views/dcf/bp_app.py
@@ -66,71 +66,7 @@ def insert_form2():
     insertData2.insert_form2(request)
     return redirect("/dcf/form2")
 
-@app.route("/dcfspreadsheet")
-def dcfspreadsheet():
-    SQL ="""
-SELECT dcf_prep_review_aprv_status.filename, COUNT(dcf_prep_review_aprv_status.filename) AS _COUNT, users.name
-FROM `dcf_prep_review_aprv_status`
-JOIN `users` ON dcf_prep_review_aprv_status.upload_by = users.id
-WHERE dcf_prep_review_aprv_status.upload_by = {}
-GROUP BY dcf_prep_review_aprv_status.filename;
-""".format(session["USER_DATA"][0]['id'])
-    uploaded_file_by_user = db.select(SQL)
-    return render_template("dcf_spreadsheet.html",user_data=session["USER_DATA"][0],uploaded_file_by_user=uploaded_file_by_user)
 
-@app.route('/form1export',methods = ['POST'])
-
-def form1export():
-    if request.method == "POST":
-        query= db.select("SELECT form_1_rcus,form_1_number_of_dips, form_1_anchor_firm, form_1_size_of_anchor, form_1_commodity, form_1_scope_provinces,form_1_for_development, form_1_cn_approved,form_1_finalized_approved,form_1_date_of_parallel_review, form_1_date_of_submission, form_1_date_of_rtwg,form_1_date_of_npco_cursory,form_1_date_of_uploading_to_ifad, form_1_date_of_ifad_no_inssuance,form_1_totalmsme, form_1_total_farmerbene,form_1_totalfo,form_1_namefo,form_1_totalhectarage_cov, form_1_hect_rehab, form_1_total_cost_rehab,form_1_hect_exp,form_1_cost_exp,form_1_euqipment,form_1_facilities, form_1_warehouse, form_1_total_matching_grant, form_1_organizational, form_1_technical_trainings, form_1_post_production, form_1_others,form_1_supply_chain_manager,form_1_y,form_1_ac,form_1_ad, form_1_ae,form_1_fmi,form_1_fmi_kms FROM dcf_prep_review_aprv_status")
-
-    df_nested_list = pd.json_normalize(query)
-    df = pd.DataFrame (df_nested_list)
-    writer = pd.ExcelWriter(c.RECORDS+'/objects/_temp_/dcf_form1_exported_file.xlsx') 
-    df.to_excel(writer, sheet_name='dcf_form1_exported_file',index=False )
-    new_column_names = 'RCUs,Number Of DIPs, Anchor Firm, Size Of Anchor, Commodity, Scope/Provinces,For development (indicate date), Date: CN Approved,Date: Full BPs and DIPs Finalized/Approved by RCUs and endorsed to NPCO for Review,Date of the Parallel Review with NPCO/RGMS-IFAD-RTWG, Date of submission of the revised DIPs based on the comments from the parallel review, Date of RTWG Approval,Date: NPCO cursory review of RCUs compliance to parallel review comments; and DIP endorsement to NOTUS Uploading,Date: Uploading to IFAD NOTUS c/o NPCO, Date: IFAD NO Issuance,Total # of MSMEs, Total # of farmer beneficiaries,Total # of FOs,Input Name of FO,Total Hectarage Covered, Hectares for Rehab, Total Cost of Rehab,Hectares for Expansion,Total Cost of Expansion,Equipment,Facilities, Warehouse, Total Matching Grant (AA+AB), Organizational, Technical Trainings, Post-Production, Others,Supply Chain Manager,Y,AC,AD, AE,FMI,FMI Kms'
-    new_column_names_list = new_column_names.split(',')
-    df.columns = new_column_names_list
-    for column in df:
-        column_width = max(df[column].astype(str).map(len).max(), len(column))
-        col_idx = df.columns.get_loc(column)
-        writer.sheets['dcf_form1_exported_file'].set_column(col_idx, col_idx, column_width);
-
-    
-    workbook  = writer.book
-    worksheet = writer.sheets['dcf_form1_exported_file']
-    header_format = workbook.add_format({'bold': True,'text_wrap': True,'valign': 'top','fg_color': '#00ace6','border': 1})
-    for col_num, value in enumerate(df.columns.values):
-        worksheet.write(0, col_num, value, header_format)
-    writer.save()
-    return send_file(c.RECORDS+'/objects/_temp_/dcf_form1_exported_file.xlsx')
-
-@app.route('/form2export',methods = ['POST'])
-
-def form2export():
-    if request.method == "POST":
-        query= db.select("SELECT form_2_rcus,form_2_pcu,form_2_commodity,form_2_dip_alignment,form_2_yes,form_2_name_owner_manager,form_2_sex_owner_manager,form_2_sector_owner_manager,form_2_business_owner_manager,form_2_partner_fo_engaged,form_2_chairperson_manager,form_2_sex_chairperson_manager,form_2_sector_chairperson_manager,form_2_office_address_province,form_2_total_number_fo,form_2_male,form_2_female,form_2_pwde,form_2_youth,form_2_ip,form_2_sc,form_2_address_of_fo_members,form_2_hectares_covered,form_2_cpa_date_signing,form_2_cpa_date_expiration,form_2_days_remaining,form_2_date_renewed,form_2_notable_cpa_incentives,form_2_remarks_status,form_2_activity_agreements,form_2_date_conducted FROM dcf_implementing_unit")
-
-    df_nested_list = pd.json_normalize(query)
-    df = pd.DataFrame (df_nested_list)
-    writer = pd.ExcelWriter(c.RECORDS+'/objects/_temp_/dcf_form2_exported_file.xlsx') 
-    df.to_excel(writer, sheet_name='dcf_form2_exported_file',index=False )
-    new_column_names = 'RCUs,Number Of DIPs, Anchor Firm, Size Of Anchor, Commodity, Scope/Provinces,For development (indicate date), Date: CN Approved,Date: Full BPs and DIPs Finalized/Approved by RCUs and endorsed to NPCO for Review,Date of the Parallel Review with NPCO/RGMS-IFAD-RTWG, Date of submission of the revised DIPs based on the comments from the parallel review, Date of RTWG Approval,Date: NPCO cursory review of RCUs compliance to parallel review comments; and DIP endorsement to NOTUS Uploading,Date: Uploading to IFAD NOTUS c/o NPCO, Date: IFAD NO Issuance,Total # of MSMEs, Total # of farmer beneficiaries,Total # of FOs,Input Name of FO,Total Hectarage Covered, Hectares for Rehab, Total Cost of Rehab,Hectares for Expansion,Total Cost of Expansion,Equipment,Facilities, Warehouse, Total Matching Grant (AA+AB), Organizational, Technical Trainings, Post-Production, Others,Supply Chain Manager,Y,AC,AD, AE,FMI,FMI Kms'
-    new_column_names_list = new_column_names.split(',')
-    df.columns = new_column_names_list
-    for column in df:
-        column_width = max(df[column].astype(str).map(len).max(), len(column))
-        col_idx = df.columns.get_loc(column)
-        writer.sheets['dcf_form2_exported_file'].set_column(col_idx, col_idx, column_width);
-
-    
-    workbook  = writer.book
-    worksheet = writer.sheets['dcf_form2_exported_file']
-    header_format = workbook.add_format({'bold': True,'text_wrap': True,'valign': 'top','fg_color': '#00ace6','border': 1})
-    for col_num, value in enumerate(df.columns.values):
-        worksheet.write(0, col_num, value, header_format)
-    writer.save()
-    return send_file(c.RECORDS+'/objects/_temp_/dcf_form2_exported_file.xlsx')
 
 @app.route('/', methods=['GET', 'POST'])
 def dcfexport_data():
