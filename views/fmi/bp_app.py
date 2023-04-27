a52a19858196b8778096d265f576fbea3724f3e6
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-04-27 13:59:09 +0800

    asd

--- views/fmi/bp_app.py
+++ views/fmi/bp_app.py
@@ -11,4 +11,22 @@ def fmi_dashboard():
 def fmi_form_a():
     return render_template("fmi_includes/forms/fmi_form_a.html",user_data=session["USER_DATA"][0])
 
+@app.route('/fmi_form_b')
+def fmi_form_b():
+    return render_template("fmi_includes/forms/fmi_form_b.html",user_data=session["USER_DATA"][0])
 
+@app.route('/fmi_form_c1')
+def fmi_form_c1():
+    return render_template("fmi_includes/forms/fmi_form_c1.html",user_data=session["USER_DATA"][0])
+
+@app.route('/fmi_form_c2')
+def fmi_form_c2():
+    return render_template("fmi_includes/forms/fmi_form_c2.html",user_data=session["USER_DATA"][0])
+
+@app.route('/fmi_form_d')
+def fmi_form_d():
+    return render_template("fmi_includes/forms/fmi_form_d.html",user_data=session["USER_DATA"][0])
+
+@app.route('/fmi_form_e')
+def fmi_form_e():
+    return render_template("fmi_includes/forms/fmi_form_e.html",user_data=session["USER_DATA"][0])
