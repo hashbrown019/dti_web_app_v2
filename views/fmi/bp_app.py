9590cdeceaeb0252e44093d25cb8818b138ce933
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-04-27 16:30:28 +0800

    asdsad

--- views/fmi/bp_app.py
+++ views/fmi/bp_app.py
@@ -30,3 +30,7 @@ def fmi_form_d():
 @app.route('/fmi_form_e')
 def fmi_form_e():
     return render_template("fmi_includes/forms/fmi_form_e.html",user_data=session["USER_DATA"][0])
+
+@app.route('/project_profile')
+def project_profile():
+    return render_template("fmi_includes/forms/project_profile.html",user_data=session["USER_DATA"][0])
