241754ee1c9d3fb67969e1a1b0e9755472363e26
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-06-07 09:30:39 +0800

    editing form 1 dcf

--- views/dcf/form_update/update_form1.py
+++ views/dcf/form_update/update_form1.py
@@ -42,10 +42,7 @@ def updateform1(request):
         form_1_post_production = request.form.get('form_1_post_production')
         form_1_others = request.form.get('form_1_others')
         form_1_supply_chain_manager = request.form.get('form_1_supply_chain_manager')
-        form_1_y = request.form.get('form_1_y')
-        form_1_ac = request.form.get('form_1_ac')
-        form_1_ad = request.form.get('form_1_ad')
-        form_1_ae = request.form.get('form_1_ae')
+        form_1_totalproject_cost = request.form.get('form_1_totalproject_cost')
         form_1_fmi = request.form.get('form_1_fmi')
         form_1_fmi_kms = request.form.get('form_1_fmi_kms')
 
@@ -55,9 +52,9 @@ def updateform1(request):
                    form_1_date_of_ifad_no_inssuance='{}', form_1_totalmsme='{}', form_1_total_farmerbene='{}', form_1_totalfo='{}', form_1_namefo='{}', form_1_totalhectarage_cov='{}',
                     form_1_hect_rehab='{}', form_1_total_cost_rehab='{}', form_1_hect_exp='{}', form_1_cost_exp='{}', form_1_euqipment='{}',
                      form_1_facilities='{}', form_1_warehouse='{}', form_1_total_matching_grant='{}', form_1_organizational='{}', form_1_technical_trainings='{}', form_1_post_production='{}',
-                      form_1_others='{}', form_1_supply_chain_manager='{}', form_1_y='{}',form_1_ac='{}', form_1_ad='{}', form_1_ae='{}', form_1_fmi='{}', form_1_fmi_kms='{}'
+                      form_1_others='{}', form_1_supply_chain_manager='{}', form_1_totalproject_cost='{}', form_1_fmi='{}', form_1_fmi_kms='{}'
                WHERE id={}
-            """.format(form_1_rcus,form_1_number_of_dips,form_1_anchor_firm,form_1_size_of_anchor,form_1_commodity,form_1_scope_provinces,form_1_for_development,form_1_cn_approved,form_1_finalized_approved,form_1_date_of_parallel_review,form_1_date_of_submission,form_1_date_of_rtwg,form_1_date_of_npco_cursory,form_1_date_of_uploading_to_ifad,form_1_date_of_ifad_no_inssuance,form_1_totalmsme,form_1_total_farmerbene,form_1_totalfo,form_1_namefo,form_1_totalhectarage_cov,form_1_hect_rehab,form_1_total_cost_rehab,form_1_hect_exp,form_1_cost_exp,form_1_euqipment,form_1_facilities,form_1_warehouse,form_1_total_matching_grant,form_1_organizational,form_1_technical_trainings,form_1_post_production,form_1_others,form_1_supply_chain_manager,form_1_y,form_1_ac,form_1_ad,form_1_ae,form_1_fmi,form_1_fmi_kms, id)
+            """.format(form_1_rcus,form_1_number_of_dips,form_1_anchor_firm,form_1_size_of_anchor,form_1_commodity,form_1_scope_provinces,form_1_for_development,form_1_cn_approved,form_1_finalized_approved,form_1_date_of_parallel_review,form_1_date_of_submission,form_1_date_of_rtwg,form_1_date_of_npco_cursory,form_1_date_of_uploading_to_ifad,form_1_date_of_ifad_no_inssuance,form_1_totalmsme,form_1_total_farmerbene,form_1_totalfo,form_1_namefo,form_1_totalhectarage_cov,form_1_hect_rehab,form_1_total_cost_rehab,form_1_hect_exp,form_1_cost_exp,form_1_euqipment,form_1_facilities,form_1_warehouse,form_1_total_matching_grant,form_1_organizational,form_1_technical_trainings,form_1_post_production,form_1_others,form_1_supply_chain_manager,form_1_totalproject_cost,form_1_fmi,form_1_fmi_kms, id)
         db.err_page = "asdasd"
         last_row_update_id = db.do(sql)
         if(last_row_update_id["response"]=="error"):
