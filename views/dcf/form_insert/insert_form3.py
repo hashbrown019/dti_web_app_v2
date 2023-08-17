ac4eb832408f58d91b7554e42b542542f2713e0b
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-08-17 13:02:26 +0800

    asd

--- views/dcf/form_insert/insert_form3.py
+++ views/dcf/form_insert/insert_form3.py
@@ -10,7 +10,6 @@ def insert_form3(request):
         upload_by = session["USER_DATA"][0]['id']
         form_3_types_of_bdsp = request.form['form_3_types_of_bdsp']
         form_3_contact_person = request.form['form_3_contact_person']
-        form_3_training_number = request.form['form_3_training_number']
         form_3_sex = request.form.get('form_3_sex')
         form_3_office_addr = request.form['form_3_office_addr']
         form_3_email = request.form['form_3_email']
@@ -35,8 +34,8 @@ def insert_form3(request):
         form_3_extension_service_facilitation = request.form.get('form_3_extension_service_facilitation')
         form_3_philgeps_registered = request.form.get('form_3_philgeps_registered')
         form_3_choices_string = ', '.join(form_3_choices)
-        form3_data = db.do("INSERT INTO dcf_bdsp_reg (upload_by,form_3_types_of_bdsp,form_3_contact_person,form_3_training_number,form_3_sex,form_3_office_addr,form_3_email,form_3_breif_description,phone,form_3_choices,form_3_preferred_region,form_3_preferred_province,form_3_name,form_3_education,form_3_expertise,form_3_prior_rapid_engagements,form_3_date_registered,form_3_rapid_implementing_unit,form_3_nature_engagements,form_3_suppliers_evaluation,form_3_other_engagement_outside_rapid,form_3_lecture_training_seminar,form_3_training_materials,form_3_organize_pool,form_3_demand_basis,form_3_extension_service_facilitation,form_3_philgeps_registered) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')". 
-        format( upload_by,form_3_types_of_bdsp,form_3_contact_person,form_3_training_number,form_3_sex,form_3_office_addr,form_3_email,form_3_breif_description,phone,form_3_choices_string,form_3_preferred_region,form_3_preferred_province,form_3_name,form_3_education,form_3_expertise,form_3_prior_rapid_engagements,form_3_date_registered,form_3_rapid_implementing_unit,form_3_nature_engagements,form_3_suppliers_evaluation,form_3_other_engagement_outside_rapid,form_3_lecture_training_seminar,form_3_training_materials,form_3_organize_pool,form_3_demand_basis,form_3_extension_service_facilitation,form_3_philgeps_registered))
+        form3_data = db.do("INSERT INTO dcf_bdsp_reg (upload_by,form_3_types_of_bdsp,form_3_contact_person,form_3_sex,form_3_office_addr,form_3_email,form_3_breif_description,phone,form_3_choices,form_3_preferred_region,form_3_preferred_province,form_3_name,form_3_education,form_3_expertise,form_3_prior_rapid_engagements,form_3_date_registered,form_3_rapid_implementing_unit,form_3_nature_engagements,form_3_suppliers_evaluation,form_3_other_engagement_outside_rapid,form_3_lecture_training_seminar,form_3_training_materials,form_3_organize_pool,form_3_demand_basis,form_3_extension_service_facilitation,form_3_philgeps_registered) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')". 
+        format( upload_by,form_3_types_of_bdsp,form_3_contact_person,form_3_sex,form_3_office_addr,form_3_email,form_3_breif_description,phone,form_3_choices_string,form_3_preferred_region,form_3_preferred_province,form_3_name,form_3_education,form_3_expertise,form_3_prior_rapid_engagements,form_3_date_registered,form_3_rapid_implementing_unit,form_3_nature_engagements,form_3_suppliers_evaluation,form_3_other_engagement_outside_rapid,form_3_lecture_training_seminar,form_3_training_materials,form_3_organize_pool,form_3_demand_basis,form_3_extension_service_facilitation,form_3_philgeps_registered))
         #return str(form3_data)
      
         if(form3_data["response"]=="error"):
--- views/dcf/form_update/update_form3.py
+++ views/dcf/form_update/update_form3.py
@@ -11,7 +11,6 @@ def updateform3(request):
         id = request.form['id']
         form_3_types_of_bdsp = request.form['form_3_types_of_bdsp']
         form_3_contact_person = request.form['form_3_contact_person']
-        form_3_training_number = request.form['form_3_training_number']
         form_3_sex = request.form.get('form_3_sex')
         form_3_office_addr = request.form['form_3_office_addr']
         form_3_email = request.form['form_3_email']
@@ -38,9 +37,9 @@ def updateform3(request):
         form_3_choices_string = ', '.join(form_3_choices)
 
         sql = """UPDATE dcf_bdsp_reg
-               SET form_3_types_of_bdsp='{}',form_3_contact_person='{}',form_3_training_number='{}',form_3_sex='{}',form_3_office_addr='{}',form_3_email='{}',form_3_breif_description='{}',phone='{}',form_3_choices='{}',form_3_preferred_region='{}',form_3_preferred_province='{}',form_3_name='{}',form_3_education='{}',form_3_expertise='{}',form_3_prior_rapid_engagements='{}',form_3_date_registered='{}',form_3_rapid_implementing_unit='{}',form_3_nature_engagements='{}',form_3_suppliers_evaluation='{}',form_3_other_engagement_outside_rapid='{}',form_3_lecture_training_seminar='{}',form_3_training_materials='{}',form_3_organize_pool='{}',form_3_demand_basis='{}',form_3_extension_service_facilitation='{}',form_3_philgeps_registered='{}'
+               SET form_3_types_of_bdsp='{}',form_3_contact_person='{}',form_3_sex='{}',form_3_office_addr='{}',form_3_email='{}',form_3_breif_description='{}',phone='{}',form_3_choices='{}',form_3_preferred_region='{}',form_3_preferred_province='{}',form_3_name='{}',form_3_education='{}',form_3_expertise='{}',form_3_prior_rapid_engagements='{}',form_3_date_registered='{}',form_3_rapid_implementing_unit='{}',form_3_nature_engagements='{}',form_3_suppliers_evaluation='{}',form_3_other_engagement_outside_rapid='{}',form_3_lecture_training_seminar='{}',form_3_training_materials='{}',form_3_organize_pool='{}',form_3_demand_basis='{}',form_3_extension_service_facilitation='{}',form_3_philgeps_registered='{}'
                WHERE id={}
-            """.format(form_3_types_of_bdsp,form_3_contact_person,form_3_training_number,form_3_sex,form_3_office_addr,form_3_email,form_3_breif_description,phone,form_3_choices_string,form_3_preferred_region,form_3_preferred_province,form_3_name,form_3_education,form_3_expertise,form_3_prior_rapid_engagements,form_3_date_registered,form_3_rapid_implementing_unit,form_3_nature_engagements,form_3_suppliers_evaluation,form_3_other_engagement_outside_rapid,form_3_lecture_training_seminar,form_3_training_materials,form_3_organize_pool,form_3_demand_basis,form_3_extension_service_facilitation,form_3_philgeps_registered, id)
+            """.format(form_3_types_of_bdsp,form_3_contact_person,form_3_sex,form_3_office_addr,form_3_email,form_3_breif_description,phone,form_3_choices_string,form_3_preferred_region,form_3_preferred_province,form_3_name,form_3_education,form_3_expertise,form_3_prior_rapid_engagements,form_3_date_registered,form_3_rapid_implementing_unit,form_3_nature_engagements,form_3_suppliers_evaluation,form_3_other_engagement_outside_rapid,form_3_lecture_training_seminar,form_3_training_materials,form_3_organize_pool,form_3_demand_basis,form_3_extension_service_facilitation,form_3_philgeps_registered, id)
         db.err_page = "asdasd"
         last_row_update_id = db.do(sql)
         if(last_row_update_id["response"]=="error"):
