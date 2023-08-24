e7c75d11bbfd764bb6850729bd130c595d8e62ea
JAY BALINO <cjmontanez221997@gmail.com>
2023-08-24 12:28:36 +0800

    asd

--- views/dcf/form_insert/insert_form9.py
+++ views/dcf/form_insert/insert_form9.py
@@ -22,8 +22,8 @@ def insert_form9(request):
         form_9_name_of_participant = ', '.join(request.form.getlist('form_9_name_of_participant[]'))
         form_9_organization = ', '.join(request.form.getlist('form_9_organization[]'))
         form_9_designation =', '.join(request.form.getlist('form_9_designation[]'))
-        form_9_sex = request.form['form_9_sex']
-        form_9_sector = request.form['form_9_sector']
+        # form_9_sex = request.form['form_9_sex']
+        # form_9_sector = request.form['form_9_sector']
         form_9_pre_test1 = request.form['form_9_pre_test1']
         form_9_post_test1 = request.form['form_9_post_test1']
         form_9_activity_output = request.form['form_9_activity_output']
@@ -32,8 +32,8 @@ def insert_form9(request):
         form_9_rating = request.form['form_9_rating']
         form_9_comments = request.form['form_9_comments']
 
-        form9_data = db.do("INSERT INTO dcf_enablers_activity (upload_by, form_9_implementing_unit,form_9_title_trade_promotion,form_9_type_of_training,form_9_others,form_9_start_date,form_9_end_date,form_9_venue,form_9_rapid_actual_budget,form_9_name_of_resource_person,form_9_name_of_participant_org,form_9_counterpart_amount,form_9_name_of_participant,form_9_organization,form_9_designation,form_9_sex,form_9_sector,form_9_pre_test1,form_9_post_test1,form_9_activity_output,form_9_pre_test2,form_9_post_test2,form_9_rating,form_9_comments) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')". 
-        format(upload_by, form_9_implementing_unit,form_9_title_trade_promotion,form_9_type_of_training,form_9_others,form_9_start_date,form_9_end_date,form_9_venue,form_9_rapid_actual_budget,form_9_name_of_resource_person,form_9_name_of_participant_org,form_9_counterpart_amount,form_9_name_of_participant,form_9_organization,form_9_designation,form_9_sex,form_9_sector,form_9_pre_test1,form_9_post_test1,form_9_activity_output,form_9_pre_test2,form_9_post_test2,form_9_rating,form_9_comments))
+        form9_data = db.do("INSERT INTO dcf_enablers_activity (upload_by, form_9_implementing_unit,form_9_title_trade_promotion,form_9_type_of_training,form_9_others,form_9_start_date,form_9_end_date,form_9_venue,form_9_rapid_actual_budget,form_9_name_of_resource_person,form_9_name_of_participant_org,form_9_counterpart_amount,form_9_name_of_participant,form_9_organization,form_9_designation,form_9_pre_test1,form_9_post_test1,form_9_activity_output,form_9_pre_test2,form_9_post_test2,form_9_rating,form_9_comments) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')". 
+        format(upload_by, form_9_implementing_unit,form_9_title_trade_promotion,form_9_type_of_training,form_9_others,form_9_start_date,form_9_end_date,form_9_venue,form_9_rapid_actual_budget,form_9_name_of_resource_person,form_9_name_of_participant_org,form_9_counterpart_amount,form_9_name_of_participant,form_9_organization,form_9_designation,form_9_pre_test1,form_9_post_test1,form_9_activity_output,form_9_pre_test2,form_9_post_test2,form_9_rating,form_9_comments))
         #return str(form5_data)
      
         if(form9_data["response"]=="error"):
--- views/dcf/form_update/update_form9.py
+++ views/dcf/form_update/update_form9.py
@@ -23,8 +23,8 @@ def updateform9(request):
         form_9_name_of_participant =', '.join(request.form.getlist('form_9_name_of_participant[]'))
         form_9_organization = ', '.join(request.form.getlist('form_9_organization[]'))
         form_9_designation = ', '.join(request.form.getlist('form_9_designation[]'))
-        form_9_sex = request.form['form_9_sex']
-        form_9_sector = request.form['form_9_sector']
+        # form_9_sex = request.form['form_9_sex']
+        # form_9_sector = request.form['form_9_sector']
         form_9_pre_test1 = request.form['form_9_pre_test1']
         form_9_post_test1 = request.form['form_9_post_test1']
         form_9_activity_output = request.form['form_9_activity_output']
@@ -34,9 +34,9 @@ def updateform9(request):
         form_9_comments = request.form['form_9_comments']
 
         sql = """UPDATE dcf_enablers_activity
-               SET form_9_implementing_unit='{}',form_9_title_trade_promotion='{}',form_9_type_of_training='{}',form_9_others='{}',form_9_start_date='{}',form_9_end_date='{}',form_9_venue='{}',form_9_rapid_actual_budget='{}',form_9_name_of_resource_person='{}',form_9_name_of_participant_org='{}',form_9_counterpart_amount='{}',form_9_name_of_participant='{}',form_9_organization='{}',form_9_designation='{}',form_9_sex='{}',form_9_sector='{}',form_9_pre_test1='{}',form_9_post_test1='{}',form_9_activity_output='{}',form_9_pre_test2='{}',form_9_post_test2='{}',form_9_rating='{}',form_9_comments='{}'
+               SET form_9_implementing_unit='{}',form_9_title_trade_promotion='{}',form_9_type_of_training='{}',form_9_others='{}',form_9_start_date='{}',form_9_end_date='{}',form_9_venue='{}',form_9_rapid_actual_budget='{}',form_9_name_of_resource_person='{}',form_9_name_of_participant_org='{}',form_9_counterpart_amount='{}',form_9_name_of_participant='{}',form_9_organization='{}',form_9_designation='{}',form_9_pre_test1='{}',form_9_post_test1='{}',form_9_activity_output='{}',form_9_pre_test2='{}',form_9_post_test2='{}',form_9_rating='{}',form_9_comments='{}'
                WHERE id={}
-            """.format(form_9_implementing_unit,form_9_title_trade_promotion,form_9_type_of_training,form_9_others,form_9_start_date,form_9_end_date,form_9_venue,form_9_rapid_actual_budget,form_9_name_of_resource_person,form_9_name_of_participant_org,form_9_counterpart_amount,form_9_name_of_participant,form_9_organization,form_9_designation,form_9_sex,form_9_sector,form_9_pre_test1,form_9_post_test1,form_9_activity_output,form_9_pre_test2,form_9_post_test2,form_9_rating,form_9_comments, id)
+            """.format(form_9_implementing_unit,form_9_title_trade_promotion,form_9_type_of_training,form_9_others,form_9_start_date,form_9_end_date,form_9_venue,form_9_rapid_actual_budget,form_9_name_of_resource_person,form_9_name_of_participant_org,form_9_counterpart_amount,form_9_name_of_participant,form_9_organization,form_9_designation,form_9_pre_test1,form_9_post_test1,form_9_activity_output,form_9_pre_test2,form_9_post_test2,form_9_rating,form_9_comments, id)
         db.err_page = "asdasd"
         last_row_update_id = db.do(sql)
         if(last_row_update_id["response"]=="error"):
