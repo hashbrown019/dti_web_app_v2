c45cba2312adf7b2c210a34c78114eeebfc79ccf
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-08-30 08:00:29 +0800

    a

--- views/dcf/form_insert/insert_form1.py
+++ views/dcf/form_insert/insert_form1.py
@@ -1,4 +1,4 @@
-from flask import flash, session
+from flask import flash, session, render_template
 import Configurations as c
 from modules.Connections import mysql
 from modules.Req_Brorn_util import file_from_request
@@ -50,10 +50,14 @@ def insert_form1(request):
         form1_data = db.do("INSERT INTO dcf_prep_review_aprv_status  (upload_by,form_1_rcus,form_1_anchor_firm,form_1_size_of_anchor,form_1_commodity,form_1_scope_provinces,form_1_for_development,form_1_cn_approved,form_1_finalized_approved,form_1_date_of_parallel_review,form_1_date_of_submission,form_1_date_of_rtwg,form_1_date_of_npco_cursory,form_1_date_of_uploading_to_ifad,form_1_date_of_ifad_no_inssuance,form_1_totalmsme,form_1_total_farmerbene,form_1_totalfo,form_1_totalhectarage_cov,form_1_hect_rehab,form_1_total_cost_rehab,form_1_hect_exp,form_1_cost_exp,form_1_euqipment,form_1_facilities,form_1_warehouse,form_1_total_matching_grant,form_1_organizational,form_1_technical_trainings,form_1_post_production,form_1_others,form_1_supply_chain_manager,form_1_totalproject_cost,form_1_fmi,form_1_fmi_kms) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')". 
         format(upload_by,form_1_rcus,form_1_anchor_firm,form_1_size_of_anchor,form_1_commodity,form_1_scope_provinces,form_1_for_development,form_1_cn_approved,form_1_finalized_approved,form_1_date_of_parallel_review,form_1_date_of_submission,form_1_date_of_rtwg,form_1_date_of_npco_cursory,form_1_date_of_uploading_to_ifad,form_1_date_of_ifad_no_inssuance,form_1_totalmsme,form_1_total_farmerbene,form_1_totalfo,form_1_totalhectarage_cov,form_1_hect_rehab,form_1_total_cost_rehab,form_1_hect_exp,form_1_cost_exp,form_1_euqipment,form_1_facilities,form_1_warehouse,form_1_total_matching_grant,form_1_organizational,form_1_technical_trainings,form_1_post_production,form_1_others,form_1_supply_chain_manager,form_1_totalproject_cost,form_1_fmi,form_1_fmi_kms))
         #return str(form5_data)
-     
+        # skkkrt = {}
+        # for key in request.form:
+        #     skkkrt[key] = request.form[key]
+        # print(skkkrt)
         if(form1_data["response"]=="error"):
             flash(f"An error occured !", "error")
-            print(form1_data)
+            # print(form1_data)
+            # return render_template('includes/forms/form1.html',user_data=session["USER_DATA"][0],data_recovery = skkkrt)
         else:
             flash(f"Record Saved!", "success")
 
