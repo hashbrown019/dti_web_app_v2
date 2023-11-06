47c9a8dbfed62d8c779ff59dde0c8d145ba94af3
JAY BALINO <cjmontanez221997@gmail.com>
2023-11-07 07:20:25 +0800

    alrighttt

--- views/dcf/dashboard/display_dataform.py
+++ views/dcf/dashboard/display_dataform.py
@@ -63,6 +63,23 @@ def displayform():
     form9_datatable=db.select("SELECT * FROM dcf_enablers_activity {} ORDER BY `id` DESC;".format(position_data_filter()))
     form10_datatable=db.select("SELECT * FROM dcf_negosyo_center {} ORDER BY `id` DESC;".format(position_data_filter()))
     form11_datatable=db.select("SELECT * FROM dcf_access_financing {} ORDER BY `id` DESC;".format(position_data_filter()))
+    dcf_form1male=db.select("SELECT SUM(form_1_totalmale) AS total_male FROM dcf_prep_review_aprv_status {}; ".format(position_data_filter()))
+    dcf_form1female=db.select("SELECT SUM(form_1_totalfemale) AS total_female FROM dcf_prep_review_aprv_status {}; ".format(position_data_filter()))
+    dcf_form2FOmale=db.select("SELECT SUM(form_2_male) AS total_male2 FROM dcf_implementing_unit {}; ".format(position_data_filter()))
+    dcf_form2FOfemale=db.select("SELECT SUM(form_2_female) AS total_female2 FROM dcf_implementing_unit {}; ".format(position_data_filter()))
+    selectdcf_form3male=db.select("SELECT form_3_sex AS total_male3 FROM dcf_bdsp_reg {} AND form_3_sex = 'male';".format(position_data_filter()))
+    selectdcf_form3female=db.select("SELECT form_3_sex AS total_female3 FROM dcf_bdsp_reg {} AND form_3_sex = 'female';".format(position_data_filter()))
+    dcf_form3male=len(selectdcf_form3male)
+    dcf_form3female=len(selectdcf_form3female)
+    dcf_form4male=db.select("SELECT SUM(cbb_total_number_per_gender_male) AS total_male4 FROM dcf_capacity_building {};".format(position_data_filter()))
+    dcf_form4female=db.select("SELECT SUM(cbb_total_number_per_gender_female) AS total_female4 FROM dcf_capacity_building {};".format(position_data_filter()))
+
+
+
+
+
+
+
     return{
         'form1_datatable':  form1_datatable,
         'form2_datatable':  form2_datatable,
@@ -74,7 +91,15 @@ def displayform():
         'form8_datatable':  form8_datatable,
         'form9_datatable':  form9_datatable,
         'form10_datatable':  form10_datatable,
-        'form11_datatable':  form11_datatable
+        'form11_datatable':  form11_datatable,
+        'dcf_form1male':  dcf_form1male,
+        'dcf_form1female':  dcf_form1female,
+        'dcf_form2FOmale':  dcf_form2FOmale,
+        'dcf_form2FOfemale':  dcf_form2FOfemale,
+        'dcf_form3male':  dcf_form3male,
+        'dcf_form3female':  dcf_form3female,
+        'dcf_form4male':  dcf_form4male,
+        'dcf_form4female':  dcf_form4female
 
     }
 
