33085e852a4e7b0a86b8fba53e9aeeb38baa4958
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-11-13 13:59:16 +0800

    Update display_dataform.py

--- views/dcf/dashboard/display_dataform.py
+++ views/dcf/dashboard/display_dataform.py
@@ -125,6 +125,7 @@ def displayform():
     over_all = {"over_all_total":0,"approve":0,"ongoing":0,"pipeline":0,"not_started":0,}
     commodities_per_status_per_region= {}
     print(" * Looping dcf f 1")
+    print(position_data_filter())
     for index in range(len(dips_list)):
         DIP = dips_list[index]
         print(DIP['form_1_rcus'])
