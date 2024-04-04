1b7f19a5ce2c7fbe1faf57e1acc75f127976d59c
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-04-04 17:20:25 +0800

    asd

--- start_point.py
+++ start_point.py
@@ -100,12 +100,13 @@ def after_request_func(response):
 			# print(request.headers)
 	# if(c.IN_MAINTENANCE):return redirect("/we_will_be_back_later")
 	# print(response.get_json())
-	response.headers.set('Strict-Transport-Security', "max-age=31536000 ")
-	# response.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'")
-	response.headers.set('X-Frame-Options', "DENY")
-	response.headers.set('X-Content-Type-Options', "nosniff")
-	response.headers.set('Referrer-Policy', "same-origin'")
-	response.headers.set('Permissions-Policy', "geolocation=(self 'none'), camera=(), microphone=()")
+	# ===============================
+	# response.headers.set('Strict-Transport-Security', "max-age=31536000 ")
+	# # response.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'")
+	# response.headers.set('X-Frame-Options', "DENY")
+	# response.headers.set('X-Content-Type-Options', "nosniff")
+	# response.headers.set('Referrer-Policy', "same-origin'")
+	# response.headers.set('Permissions-Policy', "geolocation=(self 'none'), camera=(), microphone=()")
 	return response
 
 	
