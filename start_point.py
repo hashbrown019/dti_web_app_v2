dd1f8166110ea987697b8587caf201db5c468230
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-04-24 09:32:30 +0800

    asd

--- start_point.py
+++ start_point.py
@@ -119,26 +119,26 @@ def after_request_func(response):
 
 
 def format_timestamp(timestamp):
-    now = datetime.now()
-    time_difference = now - timestamp
-    if time_difference.total_seconds() < 1:
-        return "Just now"
-    elif time_difference.total_seconds() == 1:
-        return "1 second ago"
-    elif time_difference.total_seconds() < 60:
-        return f"{int(time_difference.total_seconds())} seconds ago"
-    elif time_difference.total_seconds() == 60:
-        return "1 minute ago"
-    elif time_difference.total_seconds() < 3600:
-        minutes_ago = int(time_difference.total_seconds() / 60)
-        return f"{minutes_ago} minute{'s' if minutes_ago != 1 else ''} ago"
-    elif time_difference.total_seconds() == 3600:
-        return "1 hour ago"
-    elif time_difference.total_seconds() < 86400:
-        hours_ago = int(time_difference.total_seconds() / 3600)
-        return f"{hours_ago} hour{'s' if hours_ago != 1 else ''} ago"
-    else:
-        return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+	now = datetime.now()
+	time_difference = now - timestamp
+	if time_difference.total_seconds() < 1:
+		return "Just now"
+	elif time_difference.total_seconds() == 1:
+		return "1 second ago"
+	elif time_difference.total_seconds() < 60:
+		return f"{int(time_difference.total_seconds())} seconds ago"
+	elif time_difference.total_seconds() == 60:
+		return "1 minute ago"
+	elif time_difference.total_seconds() < 3600:
+		minutes_ago = int(time_difference.total_seconds() / 60)
+		return f"{minutes_ago} minute{'s' if minutes_ago != 1 else ''} ago"
+	elif time_difference.total_seconds() == 3600:
+		return "1 hour ago"
+	elif time_difference.total_seconds() < 86400:
+		hours_ago = int(time_difference.total_seconds() / 3600)
+		return f"{hours_ago} hour{'s' if hours_ago != 1 else ''} ago"
+	else:
+		return timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 
 
