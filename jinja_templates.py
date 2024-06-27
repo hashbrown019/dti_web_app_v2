b47fd7b86213f7a9c5ec5ebe76d4d2937caca7f6
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-06-27 11:58:28 +0800

    Update jinja_templates.py

--- jinja_templates.py
+++ jinja_templates.py
@@ -46,7 +46,7 @@ class templates:
 			res= db.select(f"SELECT * FROM `dcf_implementing_unit` WHERE `id`= '{form_2_recent_cpa}' ;")
 
 			if(len(res)==0):
-				return f"RAPID_CPA_NONE: NONE"
+				return {"status":"Old Version Format","note":"No Date Available (You may update the CPA while on draft)","cpa":"Old Format || Initial CPA "}
 			else:
 				res= db.select(f"SELECT * FROM `dcf_implementing_unit` WHERE `id`= '{form_2_recent_cpa}' ;")[0]
 				cpa_note = f"Renewal for RAPID_CPA_{res['id']} : {res['form_2_businessname']}"
