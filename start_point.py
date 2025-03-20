7b4dab20b6bcf08268dea1b6ab8be72cfa545c11
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2025-03-20 15:29:08 +0800

    Update start_point.py

--- start_point.py
+++ start_point.py
@@ -132,11 +132,12 @@ def after_request_func(response):
 	# ===============================
 	response.headers.set('Strict-Transport-Security', "max-age=31536000 ")
 	# response.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'")
-	# response.headers.set('X-Frame-Options', "DENY")
+	response.headers.set('X-Frame-Options', "DENY")
 	response.headers.set('X-Content-Type-Options', "nosniff")
 	# response.headers.set('Referrer-Policy', "same-origin'")
 	response.headers.set('Permissions-Policy', "geolocation=(self 'none'), camera=(), microphone=()")
 	return response
+
 
 
 # Minify(app=app, html=True, js=True, cssless=True)
